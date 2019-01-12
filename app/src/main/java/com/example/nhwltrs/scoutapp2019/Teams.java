781c4c2a4d73c9838dc7e02af342c9d196fdb725
nhwltrs <nhwltrs>
2019-01-12 14:01:04 -0500

    Added the Teams activity

--- app/src/main/java/com/example/nhwltrs/scoutapp2019/Teams.java
+++ app/src/main/java/com/example/nhwltrs/scoutapp2019/Teams.java
@@ -2,12 +2,63 @@ package com.example.nhwltrs.scoutapp2019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Teams extends AppCompatActivity {
 
+    ListView list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Create the Teams activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teams);
+
+        //Create the button and list
+        Button newTeam = (Button) findViewById(R.id.newTeamsButton);
+        list = (ListView) findViewById(R.id.teamsListView);
+
+        //Set a listener for the items in the list
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String teamNumber = ((TextView) view.findViewById(R.id.teamNumberTextView)).getText().toString();
+                //Match information code goes here
+
+            }
+        });
+
+        //Set a listener for the newTeam button
+        newTeam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               //Start Match Information activity
+            }
+        });
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        //Formatting for the list
+        List<HashMap<String, String>> listItems = new ArrayList<>();
+        SimpleAdapter adapter = new SimpleAdapter(this, listItems, R.layout.list_item, new String[]{"First Line", "Second Line"}, new int[]{R.id.teamNameTextView, R.id.teamNumberTextView});
+
+        list.setAdapter(adapter);
+
+        //Bluetooth code goes here
+
+
+        adapter.notifyDataSetChanged();
     }
 }
