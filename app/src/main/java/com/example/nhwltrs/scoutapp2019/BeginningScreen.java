984c4829163f4b31bf907e6fec26d01c85c837ca
Deric Dinu Daniel <dericdinu03@gmail.com>
2019-01-14 20:02:59 -0500

    Testing deric's computer

--- app/src/main/java/com/example/nhwltrs/scoutapp2019/BeginningScreen.java
+++ app/src/main/java/com/example/nhwltrs/scoutapp2019/BeginningScreen.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class BeginningScreen extends AppCompatActivity {
-
+    //deric was here
     private boolean started=false;
 
     @Override
