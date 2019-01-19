7ca55f063be650aff4a75c813c53b6ff9a146dd3
nhwltrs <nhwltrs>
2019-01-19 10:49:35 -0500

    Created some of the bluetooth files

---
+++ app/src/main/java/com/example/nhwltrs/scoutapp2019/BluetoothClass.java
@@ -0,0 +1,12 @@
+package com.example.nhwltrs.scoutapp2019;
+
+import android.bluetooth.BluetoothDevice;
+import android.os.Handler;
+import android.util.Log;
+
+/**
+ * Created by nhwlt on 1/19/2019.
+ */
+
+public class BluetoothClass {
+}
